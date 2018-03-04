3529400105e082f8d982d4f2caeb8e5d03a9e320
Gloria <gloriaewashington@gmail.com>
2018-03-03 20:09:52 -0500

    Pushing persistence against

--- NYC CribCheck/ViewControllers/SearchHistoryViewController.swift
+++ NYC CribCheck/ViewControllers/SearchHistoryViewController.swift
@@ -19,6 +19,8 @@ class SearchHistoryViewController: UIViewController {
         }
     }
     
+    var currentViolationArr = [Violation]()
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         setUpTV()
@@ -59,33 +61,42 @@ extension SearchHistoryViewController: UITableViewDelegate {
     func tableView(_ tableView: UITableView, didSelectRowAt indexPath: IndexPath) {
         //Clicked cell segues to Violation View Controller
         let locationRequest = searchHistory[indexPath.row]
-        HousingAPIClient.manager.getViolations(usingLocation: locationRequest) { result in
+        let location = LocationRequest(borough: locationRequest.borough, houseNumber: locationRequest.houseNumber, streetName: locationRequest.streetName, apartment: locationRequest.apartment, zipCode: locationRequest.zipCode)
+        
+        HousingAPIClient.manager.getViolations(usingLocation: location) { result in
             switch result {
             case .success(let onlineViolations):
                 if onlineViolations.isEmpty {
                     self.showAlert(title: "No violations", message: "This address contains no violations. Please check the address or try a different one.")
                 }
-        
+                    
                 else {
-                    let violationVC = MainTableViewController()
-                    self.performSegue(withIdentifier: "ViolationsSegue", sender: self)
-                
+                    let storyboard = UIStoryboard(name: "Main", bundle: nil)
+                    let violationVC = storyboard.instantiateViewController(withIdentifier: "MainTableViewController") as! MainTableViewController
+                    self.currentViolationArr = onlineViolations
+                    self.present(violationVC, animated: true, completion: nil)
                 }
                 
             case .failure(let error):
                 self.showAlert(title: "Error", message: error.localizedDescription )
-                }
+            }
         }
         
     }
+    
+//    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+//        if segue.destination is MainTableViewController {
+//            let mainTableVC = segue.destination as? MainTableViewController
+//            mainTableVC?.violationsArr = currentViolationArr
+//        }
+//    }
 }
 
 extension SearchHistoryViewController: UITableViewDataSource {
     func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
         return searchHistory.count
     }
-    
-    
+       
     func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
         let locationRequest = searchHistory[indexPath.row]
         let cell = tableView.dequeueReusableCell(withIdentifier: "SearchHistoryCell", for: indexPath) as! SearchHistoryTableViewCell
